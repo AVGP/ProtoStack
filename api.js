dc4e626888185203c61fa2973b58749d735d630d
Martin Naumann <martin.naumann@centralway.com>
2013-12-23 11:44:20 +0100

    Refactoring the API job code and adding the networks stuff + API URL

--- api.js
+++ api.js
@@ -1,69 +1,38 @@
-document.getElementById("connect").addEventListener("click", function() {
-  connectAPI(document.getElementById("key").value, document.getElementById("secret").value, function(apiClient) {
-    apiClient.exec("listTemplates", {templatefilter: "executable"}, function(err, res) {
-      console.log(err, res);
-      if(err) {
-        alert("ERROR: " + err.message)
-        return
-      }
-      
-      var listElem = document.getElementById("templates");
-      for(var i=0, len=res.template.length;i<len;i++) {
-        var tpl = res.template[i];
-        listElem.innerHTML += "<option value=" + tpl.id + ">" + tpl.name + "</option>"
-      }
-    });
-    
-    apiClient.exec("listZones", {}, function(err, res) {
-      console.log(err, res);
-      if(err) {
-        alert("ERROR: " + err.message)
-        return
-      }
-      
-      var listElem = document.getElementById("zones");
-      for(var i=0, len=res.zone.length;i<len;i++) {
-        var tpl = res.zone[i];
-        listElem.innerHTML += "<option value=" + tpl.id + ">" + tpl.name + "</option>"
-      }
-    });
 
-    apiClient.exec("listServiceOfferings", {}, function(err, res) {
-      console.log(err, res);
-      if(err) {
-        alert("ERROR: " + err.message)
-        return
-      }
+
+var listAvailableOptionsFor = function(apiClient, jobName, outputElementId, responsePropertyName, params) {
+  apiClient.exec(jobName, params || {}, function(err, res) {
+    console.log(err, res);
+    if(err) {
+      alert("ERROR: " + err.message)
+      return
+    }
       
-      var listElem = document.getElementById("services");
-      for(var i=0, len=res.serviceoffering.length;i<len;i++) {
-        var tpl = res.serviceoffering[i];
-        listElem.innerHTML += "<option value=" + tpl.id + ">" + tpl.name + "</option>"
-      }
-    });
+    var listElem = document.getElementById(outputElementId);
+    for(var i=0, len=res[responsePropertyName].length;i<len;i++) {
+      var item = res[responsePropertyName][i];
+      listElem.innerHTML += "<option value=" + item.id + ">" + item.name + "</option>"
+    }
+  });
+};
+
+document.getElementById("connect").addEventListener("click", function() {
+  connectAPI(document.getElementById("url").value, document.getElementById("key").value, document.getElementById("secret").value, function(apiClient) {
+    
+    listAvailableOptionsFor(apiClient, "listTemplates", "templates", "template", {templatefilter: "executable"});
+    listAvailableOptionsFor(apiClient, "listZones", "zones", "zone");
+    listAvailableOptionsFor(apiClient, "listNetworks", "networks", "network");
+    listAvailableOptionsFor(apiClient, "listServiceOfferings", "services", "serviceoffering");
     
     document.getElementById("deploy").addEventListener("click", function() {
       apiClient.exec("deployVirtualMachine", {
         serviceofferingid: document.getElementById("services").value,
         templateid: document.getElementById("templates").value,
-        zoneid: document.getElementById("zones").value
+        zoneid: document.getElementById("zones").value,
+        networkids: [ document.getElementById("networks").value ]  
       }, function(err, res) {
         console.log(err, res);
       });
     });
   });
 });
-
-/*
-var Cloudstack = require("cloudstack");
-
-var client = new Cloudstack({
-  apiUri: "https://api.exoscale.ch/compute",
-  apiKey: "DyrDvwhlFk7ZpaLJBVQOMpu1PpnHF12dD8GsKsUxq1CxmUZobgdXhGSL6rUvy4h8a5lYvx5QNyjEKWUbX2FlVw",
-  apiSecret: "bUJ434BnwQ1b1603xYCLxrSrxSghazNnB0dkE7FEjeZJ3C03fgfwyNTdXamKYmfrWEpDP7DJkeODcr82fNjkww"
-});
-
-client.exec("listTemplates", {templatefilter: "executable"}, function(err, res) {
-  console.log(err, res);
-});
-*/

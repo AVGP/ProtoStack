46f9f55f5360eb6d52df7bdf08ab31aea66d0a7c
Martin Naumann <martin.naumann@centralway.com>
2013-12-23 15:06:03 +0100

    Refactored the api.js a bit more

--- api.js
+++ api.js
@@ -1,6 +1,14 @@
+var listAsOptions = function(item) {
+    console.log(item);
+  return "<option value=" + item.id + ">" + item.name + "</option>";
+};
 
+var listAsItems = function(item) {
+    console.log(item);
+  return "<li>" + item.name + " (" + item.id + ")</option>"; 
+};
 
-var listAvailableOptionsFor = function(apiClient, jobName, outputElementId, responsePropertyName, params) {
+var listAvailableOptionsFor = function(apiClient, jobName, outputElementId, responsePropertyName, params, displayFunc) {
   apiClient.exec(jobName, params || {}, function(err, res) {
     console.log(err, res);
     if(err) {
@@ -8,10 +16,12 @@ var listAvailableOptionsFor = function(apiClient, jobName, outputElementId, resp
       return
     }
       
+    if(!displayFunc) displayFunc = listAsOptions;
+      
     var listElem = document.getElementById(outputElementId);
     for(var i=0, len=res[responsePropertyName].length;i<len;i++) {
       var item = res[responsePropertyName][i];
-      listElem.innerHTML += "<option value=" + item.id + ">" + item.name + "</option>"
+      listElem.innerHTML += displayFunc.call(this, item);
     }
   });
 };
@@ -23,6 +33,7 @@ document.getElementById("connect").addEventListener("click", function() {
     listAvailableOptionsFor(apiClient, "listZones", "zones", "zone");
     listAvailableOptionsFor(apiClient, "listNetworks", "networks", "network");
     listAvailableOptionsFor(apiClient, "listServiceOfferings", "services", "serviceoffering");
+    listAvailableOptionsFor(apiClient, "listVirtualMachines", "vms", "virtualmachine", {}, listAsItems);
     
     document.getElementById("deploy").addEventListener("click", function() {
       var vmOpts = {
@@ -31,9 +42,26 @@ document.getElementById("connect").addEventListener("click", function() {
         zoneid: document.getElementById("zones").value,
         networkids: [ document.getElementById("networks").value ]
       };
+      
       if(document.getElementById("ipaddress").value != "") vmOpts.ipaddress = document.getElementById("ipaddress").value;
+        
       apiClient.exec("deployVirtualMachine", vmOpts, function(err, res) {
         console.log(err, res);
+        setTimeout(function queryJobState() {
+          apiClient.exec("listAsyncJobStatus", { jobid: res.jobid }, function(err, jobState) {
+            console.log(err, res);
+            if(err) {
+              console.log(err);
+              return;
+            }
+              
+            var listElem = document.getElementById("vms");
+            var vmInfo = jobState.jobresult.virtualmachine;
+            listElem.innerHTML += "<li>" + vmInfo.displayname + "(" + vmInfo.nic[0].ipaddress + ")" + "</li>";
+            //if the job is still pending, rerun this!
+            if(res.jobstatus == 0) setTimeout(queryJobState, 1000);
+          });
+        }, 1000);
       });
     });
   });

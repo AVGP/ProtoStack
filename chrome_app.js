5b561b53b58dc7fac7c3de0a6869987edc01badf
Martin Naumann <martin.naumann@centralway.com>
2013-12-23 11:48:56 +0100

    A wider window ftw (for the window)

--- chrome_app.js
+++ chrome_app.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'bounds': {
-      'width': 800,
+      'width': 950,
       'height': 600
     }
   });
